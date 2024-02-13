1267de8ece8e222db5b91956fcff2e9b0b7af3fc
OuraD <ouradiarra2@gmail.com>
2024-02-13 16:56:44 +0100

    algorithme

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// Un commentaire en JavaScript
+
+/*
+Bcp de commentaire, plein de ligne 
+plein plein 
+plein pleeeein
+*/
+
+/* ------------------------- Affichage------------------------------*/
+
+// Afficher des info dans la console du navigateur
+console.log('Hello World, This is me, Life should be')
+
+/* ------------------------- Affichage------------------------------*/
+
+/* ------------------------- Les variables ------------------------------*/
+// Declaration d'une variable
+let uneVariable
+// Affectation d'une variable 
+uneVariable = "Je suis une variable!"
+console.log(uneVariable)
+
+let hellofriends = "Hello Friends love"
+
+console.log(hellofriends)
+
+// Les différents types de variable
+let messages = 'un message'
+let number = 20
+let decimal = 20.5
+let boolean = true
+let boolean2 = false
+
+// Les opérations arithmétiques (+,-,*,/)
+// Déclarer 3 variables sum, number1, number2
+// Affecter le nombre 10 à number1
+// Affecter le nombre 20 à number2
+// Affecter à sum l'opération de multiplication des 2 variables précédentes
+
+let sum 
+let number1 = 10
+let number2 = 20
+sum = number1 * number2w
+console.log(sum)
+
+/* ------------------------- La Concatenation ------------------------------*/
+let firstName = 'Oura'
+let lastName = 'Diarra'
+
+// Je m'appelle firstName lastName
+
+console.log("Je m'appelle " + firstName +' '+ lastName)
+
+// Déclarer une variable city, afficher message j'adore vivre à city
+ let City = 'Plessis Bouchard'
+console.log("J'adore vivre au " + City)
+
+console.log(`J'adore vivre au ${City}, je m'appelle ${firstName} ${lastName}`)
